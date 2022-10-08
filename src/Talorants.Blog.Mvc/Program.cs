6ff368faad93a6d80ce4326b80dd6d8ff2aa0634
Leverow <info.enjoymedia@mail.ru>
2022-10-09 03:31:42 +0500

    Added DbContext

--- src/Talorants.Blog.Mvc/Program.cs
+++ src/Talorants.Blog.Mvc/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Talorants.Blog.Mvc.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options
+    => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
