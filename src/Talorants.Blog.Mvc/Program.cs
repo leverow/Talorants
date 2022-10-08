7d163d2b5258566cff78088cbc8e8772496a418f
Leverow <info.enjoymedia@mail.ru>
2022-10-09 03:52:11 +0500

    Identity server configured

--- src/Talorants.Blog.Mvc/Program.cs
+++ src/Talorants.Blog.Mvc/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Talorants.Blog.Mvc.Data;
+using Talorants.Blog.Mvc.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,19 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options
     => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+    
+    options.User.RequireUniqueEmail = false;
+    options.SignIn.RequireConfirmedEmail = false;
+})
+.AddRoles<IdentityRole>()
+.AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
