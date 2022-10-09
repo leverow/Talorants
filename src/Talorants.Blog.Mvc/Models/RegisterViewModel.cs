fb397cf2b563e80a5ecd65b2b1f832b87c114fd2
Leverow <info.enjoymedia@mail.ru>
2022-10-10 00:23:08 +0500

    Register ViewModel added

---
+++ src/Talorants.Blog.Mvc/Models/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Talorants.Blog.Mvc.Models;
+
+public class RegisterViewModel
+{
+    [Required(ErrorMessage = "FullName is required.")]
+    public string? FullName { get; set; }
+
+    [EmailAddress]
+    public string? Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    [DataType(DataType.Password)]
+    public string? Password { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string? ConfirmPassword { get; set; }
+
+    public string? ReturnUrl { get; set; }
+
+    [Required( ErrorMessage = "Username is required")]
+    public string? Username { get;  set; }
+}
