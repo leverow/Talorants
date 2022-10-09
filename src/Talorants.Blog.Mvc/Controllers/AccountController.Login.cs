dd1918bd30c1f7b7c8c849178666c7ba31173e81
Leverow <info.enjoymedia@mail.ru>
2022-10-10 04:17:19 +0500

    Login action added

--- src/Talorants.Blog.Mvc/Controllers/AccountController.Login.cs
+++ src/Talorants.Blog.Mvc/Controllers/AccountController.Login.cs
@@ -5,23 +5,23 @@ namespace Talorants.Blog.Mvc.Controllers;
 
 public partial class AccountController
 {
-    // public IActionResult Login(string? returnUrl)
-    // => View(new LoginViewModel() {ReturnUrl = returnUrl});
+    public IActionResult Login(string? returnUrl)
+    => View(new LoginViewModel() {ReturnUrl = returnUrl});
 
-    // [HttpPost]
-    // public async Task<IActionResult> Login(LoginViewModel model)
-    // {
-    //     if(!ModelState.IsValid) return View(model);
-    //     var signInResult = await _signInManager.PasswordSignInAsync(model.Username, model.Password, false, false);
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+        if(!ModelState.IsValid) return View(model);
+        var signInResult = await _signInManager.PasswordSignInAsync(model.Username, model.Password, false, false);
 
-    //     if(!signInResult.Succeeded) return View(model);
+        if(!signInResult.Succeeded) return View(model);
 
-    //     return LocalRedirect($"{model.ReturnUrl ?? "/"}");
-    // }
+        return LocalRedirect($"{model.ReturnUrl ?? "/"}");
+    }
 
-    // public async Task<IActionResult> Logout()
-    // {
-    //     await _signInManager.SignOutAsync();
-    //     return RedirectToAction(nameof(Login));
-    // }
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction(nameof(Login));
+    }
 }
--- src/Talorants.Blog.Mvc/Models/LoginViewModel.cs
+++ src/Talorants.Blog.Mvc/Models/LoginViewModel.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Talorants.Blog.Mvc.Models;
 
 public class LoginViewModel
 {
+    [Required(ErrorMessage = "Username is required.")]
     public string? Username { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    [DataType(DataType.Password)]
     public string? Password { get; set; }
 
     public string? ReturnUrl { get; set; }
